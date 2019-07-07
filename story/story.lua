55c807c642dad8c0071a789f0aeae39ff2615acb
MadShadow- <Mad.Shadow@web.de>
2019-07-07 21:10:48 +0200

    Map & Story update

---
+++ story/story.lua
@@ -0,0 +1,54 @@
+#############################################################################
+Story und Ideen
+
+
+##########################################################################################################################################################
+	Geschichte
+##########################################################################################################################################################
+Helias, Salim, Pilgrim und Yuki haben Kunde erhalten, dass Erec und Drake gefangen genommen, 
+dass ein fieser Baron sie gefangen genommen hat und hinrichten lassen will. Er ist für seine Grausamkeiten
+bekannt und wird nicht mit sich reden lassen. Während Helias und Pilgrim aus dem Südosten kommen, erreichen
+Salim und Yuki die Gegend aus dem Westen.
+
+
+##########################################################################################################################################################
+	Abschnitt 1: Siedlungsaufbau
+##########################################################################################################################################################
+	- Der Spieler startet mit einem Held, einer Handvoll Leibeigener und kann seine Burg frei platzieren
+	- Banditen und Räuber machen die Wälder unsicher
+	- Die Burg muss abseits der Straßen errichtet werden - die dort patroullierenden Gruppen würden sonst sofort Alarm schlagen.
+	- Falls die Truppen Alarm schlagen wird der Spieler sofort von einer Horde schwerer Kavallerie attackiert.
+	- Technologien anfangs stark beschränkt (nur max. stufe 2 truppen)
+	- Keine Türme baubar
+	- Kein Handel möglich
+	
+	- Es gibt Vororte in denen die Spieler Quests erledigen können und dafür im Augleich Dinge erhalten
+	- Technologien, Items, Informationen
+	
+	
+	- Ziel: Die Spieler finden sich zusammen und können den Bergpass überqueren um dann die Tore zu öffnen
+	
+
+##########################################################################################################################################################
+	Abschnitt 2: Kampf zusammen gegen den Feind
+##########################################################################################################################################################
+
+
+##########################################################################################################################################################
+	Abschnitt 3: Siedlungsaufbau
+##########################################################################################################################################################
+
+
+##########################################################################################################################################################
+	Abschnitt 4: Siedlungsaufbau
+##########################################################################################################################################################
+
+
+##########################################################################################################################################################
+	Abschnitt 5: Siedlungsaufbau
+##########################################################################################################################################################
+
+
+NPC Namen:
+{"Hunter", "Der Eisenfresser", "Master", "Benoron", "Parzival", "Landlord", "Simi", "Napo", "Mad", "Pit", "Minerva",
+"Silbergolem", "Yannick", "Prosperity", "Jayzee", "Bene", "deere", "Akaito", "Alcrib", "Phantom", "Yannick", } -- ...
